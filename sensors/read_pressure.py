8583ee732f453fccc9d3ad1b4b6be05ae4005425
Satvik Chaudhary <52381290+SatvikSC@users.noreply.github.com>
2022-06-06 15:29:59 +0530

    Update read_pressure.py
    if-else

--- sensors/read_pressure.py
+++ sensors/read_pressure.py
@@ -7,5 +7,10 @@ import time
 time.sleep(1)
 bmp = BMP085.BMP085()
 
-print('P = {0:0.2f} Pa'.format(bmp.read_pressure()))
-print('C.L. P = {0:0.2f} Pa'.format(bmp.read_sealevel_pressure()))
+pressure = bmp.read_pressure()
+sealevel_pressure = bmp.read_sealevel_pressure()
+
+if pressure is not None:
+    print('P = {0:0.2f} Pa  C.L. P = {0:0.2f} Pa'.format(pressure, sealevel_pressure))
+else:
+    print('Fail to read. Try again!')
