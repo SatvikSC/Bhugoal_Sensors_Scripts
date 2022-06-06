85e46d0996bb8be5909bdd73049a68a006717ba1
Satvik Chaudhary <52381290+SatvikSC@users.noreply.github.com>
2022-06-06 16:02:58 +0530

    Update read_hum.py

--- sensors/read_hum.py
+++ sensors/read_hum.py
@@ -9,6 +9,6 @@ Pin=22
 hum, _ = Adafruit_DHT.read_retry(Device, Pin)
 
 if hum is not None:
-    print('Humidity={0.1f}%'.format(hum))
+    print('Humidity={0:0.1f}%'.format(hum))
 else:
     print('Fail to read. Try again!')
