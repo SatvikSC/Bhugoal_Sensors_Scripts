6c690760c2358396e50bbc3266c4d8e8287d60a3
Satvik Chaudhary <52381290+SatvikSC@users.noreply.github.com>
2022-06-06 15:59:12 +0530

    Update read_signal.py

--- sensors/read_signal.py
+++ sensors/read_signal.py
@@ -10,7 +10,7 @@ from time import sleep
 i2c = busio.I2C(3,2)
 ads = ADS.ADS1115(i2c)
 
-while 1:
+for _ in range(1,11):
     chan = AnalogIn(ads,ADS.P1)
     print(round(chan.voltage,5), round(chan.voltage*6))
     sleep(1)
